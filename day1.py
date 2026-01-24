4028c11254a50fa3dee1b06bf04e976dd68c0793
bishal-svg <bishalbaibhav40@gmail.com>
2026-01-24 13:03:13 +0000

    first commit

---
+++ day1.py
@@ -0,0 +1,30 @@
+#python is the programming languages 
+#it helps us to operate our computer
+
+
+# we use "# " to do single line comment
+# we use " ''' -- ''' " to do multi line comment
+ 
+# we use "\n " for escaping line
+# we use " \" " for escaping double quotes
+# we use "\ '  " for escaping single quotes
+
+# we use print(a,b,c,sep=~,end=009\n) output:-a~b~c009 then next line
+ 
+#we use int, string , boolean,list,tuple,dictionary(dict), null and other datatypes 
+#we use (type(datatype))we should use to check datatype
+
+# type conversion
+
+''' 
+implicit conversion
+ type conversion done by user:-
+ ex :int(a)
+
+ explicit :-
+ type conversion done by compiler
+ ex: 8+0.9 output :- 8.9
+
+'''
+
+# input take string data 
