a6aa8754253fbcdf44f26127526ed4ad2ac8196d
bishal-svg <bishalbaibhav40@gmail.com>
2026-01-26 06:48:19 +0000

    functions,loops,conditions notes

---
+++ day3.py
@@ -0,0 +1,60 @@
+'''
+conditional 
+1) if else
+if(condition):
+    code
+else:
+
+2)match case:
+match expression:
+  
+  case expression:
+    code
+   case _: 
+   default
+
+loop
+ 1)for i in range(start,stop,step) :
+    print i
+
+ 2) for i in character:     it is for string list tuple and dict
+ print i
+
+ 3) i=0
+  while(i<5):
+    code or something 
+
+4) induced do while loop
+   while True
+   if ( condition):       when condition get true programm stops               
+
+break
+
+
+break stop 
+continue skips 
+
+function:
+
+it defined by def and called by name
+
+def name ():
+    return code
+
+name( arguments)
+
+*arguments=becomes list
+**arguments=becomes dict
+name(arguments=value)  order doesn't matter at all
+
+
+
+
+
+
+
+
+
+'''
+
+ 
